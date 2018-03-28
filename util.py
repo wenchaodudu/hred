b01ab6dc5a2ae5346fd1d4839acc05c407918401
Xianyang <chenxianyang.tom@gmail.com>
2018-03-27 23:23:40 -0400

    commit

--- util.py
+++ util.py
@@ -1,5 +1,9 @@
 import numpy as np
 
 
-def load_word_embedding(path):
-    pass
+def get_length(data):
+    len_turns = list(map(lambda x: np.shape(np.where(x[:,0]>0))[1], data))
+    len_uttrs = []
+    for d in data:
+        len_uttrs += list(map(lambda x: np.shape(np.where(x>0))[1], d))
+    return len_turns, len_uttrs
