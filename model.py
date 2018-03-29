50b73b4ec45677cc288ab606e20be1b6649ce7e3
Xianyang <chenxianyang.tom@gmail.com>
2018-03-29 00:37:50 -0400

    embedding

--- model.py
+++ model.py
@@ -21,10 +21,10 @@ class Embedding(nn.Module):
         # init_embedding: 2d matrix of pre-trained word embedding
         # row 0 used for padding
         super(Embedding, self).__init__()
-        self.num_embeddings, self.embedding_dim = num_embeddings + 1, embedding_dim
+        self.num_embeddings, self.embedding_dim = num_embeddings, embedding_dim
         self.weight = Parameter(torch.Tensor(self.num_embeddings, self.embedding_dim))
 
-        self.padding_idx = 0
+        self.padding_idx = None
         self.max_norm = None
         self.norm_type = 2
         self.scale_grad_by_freq = False
@@ -35,7 +35,7 @@ class Embedding(nn.Module):
 
     def reset_parameters(self, init_embedding=None):
         if not (init_embedding is None):
-            self.weight[1:].data.copy_(torch.from_numpy(init_embedding))
+            self.weight.data.copy_(torch.from_numpy(init_embedding))
         else:
             self.weight.data.normal_(0, 1)
         if self.padding_idx is not None:
