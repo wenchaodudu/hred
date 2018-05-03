feee85579af59e4b801ed00b4a32e9cc1e4158aa
Xianyang <chenxianyang.tom@gmail.com>
2018-05-03 17:29:48 -0400

    add

--- test_disc.py
+++ test_disc.py
@@ -10,18 +10,6 @@ def main():
     dictionary = json.load(open('./dictionary.json'))
     vocab_size = len(dictionary) + 1
 
-    word_embedding_dim = 300
-    print("Vocabulary size:", len(dictionary))
-    word_vectors = np.random.uniform(low=-0.1, high=0.1, size=(vocab_size, word_embedding_dim))
-    found = 0
-    print("Loading word vecotrs.")
-    word2vec = Word2Vec.load('./word2vec.vector')
-    for word in word2vec.wv.vocab:
-        if word in dictionary:
-            word_vectors[dictionary[word]] = word2vec.wv[word]
-            found += 1
-    print(found)
-
     train_loader = get_loader('./data/train.src', './data/train.tgt', dictionary, 80)
 
     disc = torch.load('discriminator.pt')
