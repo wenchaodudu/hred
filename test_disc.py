710b9c33c7c737bfb2e1b8f74dbccc8d68511e0c
Xianyang <chenxianyang.tom@gmail.com>
2018-05-04 21:12:12 -0400

    commit

--- test_disc.py
+++ test_disc.py
@@ -9,6 +9,9 @@ from hred_data_loader import get_loader
 def main():
     dictionary = json.load(open('./dictionary.json'))
     vocab_size = len(dictionary) + 1
+    inverse_dict = {}
+    for word, wid in dictionary.items():
+        inverse_dict[wid] = word
 
     train_loader = get_loader('./data/train.src', './data/train.tgt', dictionary, 80)
 
@@ -17,9 +20,14 @@ def main():
 
     for _, (src_seqs, src_lengths, src_indices, ctc_seqs, ctc_lengths, ctc_indices, trg_seqs, trg_lengths,
             trg_indices, turn_len) in enumerate(train_loader):
-        print(trg_seqs)
         scores = disc.evaluate(ctc_seqs, ctc_lengths, ctc_indices, trg_seqs, trg_lengths, trg_indices)
-        print(scores)
+        for i in range(scores.shape[0]):
+            print(reconstruct_sent(trg_seqs[i], inverse_dict))
+            print(scores[i])
+
+
+def reconstruct_sent(seq, dictionary):
+    return ' '.join([dictionary[wid] for wid in seq])
 
 
 if __name__ == '__main__':
