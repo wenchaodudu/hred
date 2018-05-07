dcf402dbc045f761103cbf16c69371faadbaf21c
Xianyang <chenxianyang.tom@gmail.com>
2018-05-06 23:10:58 -0400

    commit

--- adversarial.py
+++ adversarial.py
@@ -74,16 +74,16 @@ def train():
 
             if trainD:
                 optim_D.zero_grad()
-                disc_label = torch.zeros(trg_seqs.size()[0])
+                disc_label = np.zeros(trg_seqs.size()[0])
                 loss = disc.loss(ctc_seqs, ctc_lengths, ctc_indices, gumbel_out, gumbel_lengths, gumbel_indices, disc_label, None)
-                disc_label = torch.ones(trg_seqs.size()[0])
+                disc_label = np.ones(trg_seqs.size()[0])
                 loss += disc.loss(ctc_seqs, ctc_lengths, ctc_indices, trg_seqs, trg_lengths, trg_indices, disc_label, None)
                 loss.backward()
                 optim_D.step()
 
             elif trainG:
                 optim_G.zero_grad()
-                disc_label = torch.ones(trg_seqs.size()[0])
+                disc_label = np.ones(trg_seqs.size()[0])
                 print(ctc_lengths, trg_lengths)
                 print(gumbel_out.size())
                 loss = disc.loss(ctc_seqs, ctc_lengths, ctc_indices, gumbel_out, gumbel_lengths, gumbel_indices, disc_label, None)
